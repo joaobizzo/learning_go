fdc8dce167ac930e4620dd952c57caf2c0c0c5c4
Joao Bizzo <brandtjoao7@gmail.com>
2023-10-26 15:27:34 -0300

    update

--- hello/hello.go
+++ hello/hello.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
@@ -26,9 +27,11 @@ func main() {
 	case 2:
 		fmt.Println("Exibindo logs...")
 	case 0:
-		fmt.Println("Saindo do programa...")
+		fmt.Println("Saindo do programa")
+		os.Exit(0)
 	default:
 		fmt.Println("Não conheço esse comando")
+		os.Exit(-1)
 	}
 }
 
